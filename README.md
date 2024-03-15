a03f66533a13a1cc61efc9d8fdfb998c1e8f742c
Anaël Delorme <87323681+anaelDelorme@users.noreply.github.com>
2024-03-15 10:14:33 +0000

    init skeleton site

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hackaviz2024
-DataVisualiaton Jeux de Paris dans le cadre de l'Hackaviz 2024 de Toulouse Dataviz
+# Hackaviz 2024 
+
+Datavisualisation sur les Jeux de Paris dans le cadre de l'Hackaviz 2024 de Toulouse DataViz
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	//Dark or Ligth
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	export const prerender = true;
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Hackaviz 2024</strong>
+				<img src="olympics.svg" alt="Olympics" class="ml-2 w-10 h-10" />
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/anaelDelorme/hackaviz2024"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
